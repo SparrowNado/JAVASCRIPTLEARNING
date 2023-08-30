fad00ce1767ad44c6264360e3bd3ae9fd12adfee
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-08-30 20:02:15 +0800

    renamed and added new file

---
+++ Printing.js
@@ -0,0 +1,16 @@
+var a = 7;
+
+var b ="Hello World";
+
+
+
+//Used to debug code
+console.log(a);
+console.log(b);
+
+//Alert only says specific text
+ alert("Hello World");
+ alert("Person says hi");
+
+//USed to  print content to a  html document
+document.write('Hello, WOrld!');
